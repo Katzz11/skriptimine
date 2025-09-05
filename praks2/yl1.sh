1e56ff772337bdaafa8332043358707920709614
Katriin Poska <katriin.poska@voco.ee>
2025-09-05 03:48:58 -0400

    kasutaja tervitus

---
+++ praks2/yl1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# see skript tervitab kasutajat
+
+echo "tere, katriin"
+
+# väljastab ter ilma reavahetuseta
+
+echo "tere,"
+
+# väljastab kasutajanimi
+
+echo "$USER"
+
+# väljastab !
+
+echo "!"
